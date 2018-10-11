3551136d477c1e4abc9f937b376092072390ca0a
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 20:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   let moreDrivers = drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  let moreDrivers = drivers.shift(name);
+}
