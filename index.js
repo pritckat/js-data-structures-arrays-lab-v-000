ddbeb5e9f29e5cc5bb2426db495fcda9443ed825
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 20:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
+
 
 function destructivelyAppendDriver(name) {
-  let drivers = drivers.push(name)
+  const drivers = ["Milo", "Otis", "Garfield"];
 }
